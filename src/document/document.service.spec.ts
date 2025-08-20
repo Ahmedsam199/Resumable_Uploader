bcc6e6ddedc98851d100be2a5dcac804e8d4d6a9
Ahmed Saman <98406575+Ahmedsam199@users.noreply.github.com>
2025-08-20 11:46:15 +0300

    update a test file

--- src/document/document.service.spec.ts
+++ src/document/document.service.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DocumentService } from './document.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UtilService } from 'src/util/util.service';
+import { MinioService } from 'src/minio/minio.service';
 
 describe('DocumentService', () => {
   let service: DocumentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DocumentService, PrismaService],
+      providers: [DocumentService, PrismaService, UtilService, MinioService],
     }).compile();
 
     service = module.get<DocumentService>(DocumentService);
