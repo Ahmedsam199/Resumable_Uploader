b9a40579705d320cc5f405b367d6259c964d3f37
Ahmed Saman <98406575+Ahmedsam199@users.noreply.github.com>
2025-08-21 12:27:33 +0300

    Adding Swagger OpenAPI to the App
    Adding Swagger OpenAPI to the App to make work easier for the users

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -15,6 +16,15 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
+  const config = new DocumentBuilder()
+    .setTitle('Resumable uploader')
+    .setDescription('The uploader API description')
+    .setVersion('1.0')
+    .addTag('upload')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
